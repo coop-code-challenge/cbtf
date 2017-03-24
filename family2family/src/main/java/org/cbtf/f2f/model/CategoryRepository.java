59ef9493cf75c4137f3b65c1a577678c1435fccb
mlievens <Matthew.Lievens@gmail.com>
2017-03-23 19:06:47 -0500

    Change repository paths to match table names

--- family2family/src/main/java/org/cbtf/f2f/model/CategoryRepository.java
+++ family2family/src/main/java/org/cbtf/f2f/model/CategoryRepository.java
@@ -3,7 +3,7 @@ package org.cbtf.f2f.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "Category", path = "Category")
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
 
 
--- family2family/src/main/java/org/cbtf/f2f/model/ChildRepository.java
+++ family2family/src/main/java/org/cbtf/f2f/model/ChildRepository.java
@@ -3,7 +3,7 @@ package org.cbtf.f2f.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "Child", path = "Child")
 public interface ChildRepository extends CrudRepository<Child, Integer> {
 
 
