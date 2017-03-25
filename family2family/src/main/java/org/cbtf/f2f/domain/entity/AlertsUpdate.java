70af631f3ad0259a36418583eb15d1f38bf1e799
Brockman <lbrockman@f-w.com>
2017-03-25 10:36:28 -0500

    Added the AlertsUpdate entity to project (creates the alerts_update table in the database).

---
+++ family2family/src/main/java/org/cbtf/f2f/domain/entity/AlertsUpdate.java
@@ -0,0 +1,54 @@
+package org.cbtf.f2f.domain.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.sql.Date;
+
+/**
+ * Created by lbrockman on 3/25/17.
+ */
+@Entity
+public class AlertsUpdate {
+
+    @Id
+    @GeneratedValue
+    private Integer alertID;
+
+    private String message;
+
+    private Date dateTime;
+
+    public AlertsUpdate() {
+
+    }
+
+    public AlertsUpdate(String message, Date dateTime) {
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+
+    public Integer getAlertID() {
+        return alertID;
+    }
+
+    public void setAlertID(Integer alertID) {
+        this.alertID = alertID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+}
