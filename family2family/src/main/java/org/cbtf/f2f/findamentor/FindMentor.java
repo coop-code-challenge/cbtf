717606e1c66e8655f5b4dfbe668b1c0685aafbd5
mikey schreiber <mikey schreiber>
2017-03-25 14:19:57 -0500

    added getting info from DB

--- family2family/src/main/java/org/cbtf/f2f/findamentor/FindMentor.java
+++ family2family/src/main/java/org/cbtf/f2f/findamentor/FindMentor.java
@@ -1,10 +1,17 @@
 package org.cbtf.f2f.findamentor;
 
+import org.cbtf.f2f.domain.entity.Child;
 import org.cbtf.f2f.domain.entity.Contact;
 import org.cbtf.f2f.domain.entity.Users;
+import org.cbtf.f2f.domain.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URLEncoder;
+import java.util.List;
 
 @RestController
 @RequestMapping("${org.cbtf.apiRoot}/findmatch")
@@ -13,12 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
  */
 public class FindMentor {
 
-    @GetMapping("/{userId}")
-    public Users findMatch(String userId) {
-        Contact tempContact = new Contact();
-        Users temp = new Users("id1234","john", "schreiber", false, true, tempContact );
+    @Autowired
+    private UsersRepository userRepo;
 
-        return temp;
+    @GetMapping("/{userId}")
+    public Users findMatch(@PathVariable("userId") String userId) {
+        Users user = userRepo.findOne(userId);
+        Contact userContact = user.getContact();
+
+        List<Child> children = userContact.getChildren();
+
+        for(int i= 0; i < children.size(); i++) {
+
+
+
+        }
+
+
+
+
+
+
+        return user;
     }
 
 
