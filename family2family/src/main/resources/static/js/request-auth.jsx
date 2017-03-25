66a48ed239f2086613b380a7d5a2477d7f17a4e4
fabirk <fbirk@siue.edu>
2017-03-25 14:47:27 -0500

    Removed nav bar from request-auth

--- family2family/src/main/resources/static/js/request-auth.jsx
+++ family2family/src/main/resources/static/js/request-auth.jsx
@@ -1,21 +1,4 @@
 
-class NavBarComponent extends React.Component {
-    render() {
-        return(
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <ul className="nav navbar-nav">
-                        <li className="active"><a href="#">Home</a></li>
-                        <li><a href="#Contact Entry">Contact Entry</a></li>
-                        <li><a href="#Find a Mentor">Find a Mentor</a></li>
-                        <li><a href="#Admin Settings">Admin Settings</a></li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
-}
-
 class AlreadySignedIn extends React.Component {
     constructor(props) {
         super(props);
@@ -43,7 +26,6 @@ class AppContent extends React.Component {
     render() {
         return (
             <div>
-                <NavBarComponent />
                 <AlreadySignedIn />
             </div>
         );
