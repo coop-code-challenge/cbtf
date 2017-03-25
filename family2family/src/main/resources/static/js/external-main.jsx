9caad10346399deaecad54d8d9032a8006a6b7a4
fabirk <fbirk@siue.edu>
2017-03-25 14:31:11 -0500

    removed nav-bar

--- family2family/src/main/resources/static/js/external-main.jsx
+++ family2family/src/main/resources/static/js/external-main.jsx
@@ -1,21 +1,3 @@
-
-class NavBarComponent extends React.Component {
-    render() {
-        return(
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <ul className="nav navbar-nav">
-                        <li className="active"><a href="#">Home</a></li>
-                        <li><a href="#Contact Entry">Contact Entry</a></li>
-                        <li><a href="#Find a Mentor">Find a Mentor</a></li>
-                        <li><a href="#Admin Settings">Admin Settings</a></li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
-}
-
 class AlreadySignedIn extends React.Component {
     constructor(props) {
         super(props);
