3f83878a42db958702f01a717fd3e6408f21e899
Tony Scott <toscott1@gmail.com>
2017-04-04 21:50:12 -0500

    Example of exporting/importing locally defined component

---
+++ family2family/src/main/web/test/mycomponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class MyComponent extends React.Component {
+    render () {
+        return <p>This is my test component</p>;
+    }
+}
--- family2family/src/main/web/test/test.jsx
+++ family2family/src/main/web/test/test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
+import MyComponent from './mycomponent.jsx';
 
 class App extends React.Component {
     render () {
         return <div>
             <p> Hello React!</p>
+            <MyComponent/>
         </div>;
     }
 }
