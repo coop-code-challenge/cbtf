47f73f5a09b3ed49cd25dfb46f8b96c13b267031
Tony Scott <toscott1@gmail.com>
2017-04-04 19:49:08 -0500

    Adding node and webpack to bundle the js

---
+++ family2family/webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var dir = "./";
+
+var BUILD_DIR = path.resolve(dir, 'src/main/resources/static/build');
+var APP_DIR = path.resolve(dir, 'client');
+
+var config = {
+    entry: APP_DIR + '/test.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders : [
+            {
+                test : /\.jsx?/,
+                include : APP_DIR,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+};
+
+module.exports = config;
