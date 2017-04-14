0742870f2a0a61e58c33d80608e39057c4bd09ac
Joe Theisman <theismanj@gmail.com>
2017-04-14 12:59:41 -0500

    created tests for using react router. Next step, to use it correctly in our application.

--- family2family/client/test.jsx
+++ family2family/client/test.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import {render} from 'react-dom';
+import React, { Component } from 'react'
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom'
 
-class App extends React.Component {
-    render () {
-        return <p> Hello React234!</p>;
+class App extends Component {
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/address' component={Address} />
+                </div>
+            </HashRouter>
+        )
     }
 }
 
-render(<App/>, document.getElementById('app'));
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are located at 555 Jackson St.</h1>
+
+export default App
--- family2family/webpack.config.js
+++ family2family/webpack.config.js
@@ -7,7 +7,7 @@ var BUILD_DIR = path.resolve(dir, 'src/main/resources/static/build');
 var APP_DIR = path.resolve(dir, 'client');
 
 var config = {
-    entry: APP_DIR + '/test.jsx',
+    entry: APP_DIR + '/router_test.jsx',
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
